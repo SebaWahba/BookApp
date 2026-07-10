817b947fdafa924b334c567aa49ba71fd50904b6
JanaMostafa <mostafajana857@gmail.com>
2026-07-10 14:12:03 +0300

    refactor: centralize colors and text styles into AppColors/AppTextStyles/AppTheme

--- lib/core/widgets/app_heading.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-class AppHeading {
- static const textColor = Color(0xFF121212);
-  static TextStyle get style => GoogleFonts.openSans(color: textColor, fontWeight: FontWeight.w700, fontSize: 24.0, height: 1.35 , letterSpacing: -0.72 ,);
-
-
-}
--- lib/core/widgets/skip_button.dart
+++ lib/core/widgets/skip_button.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import '../../config/themes/app_text_styles.dart';
+import '../../config/themes/app_colors.dart';
 
 class SkipButton extends StatelessWidget {
- static const textColor = Color(0xFF54408C);
- final VoidCallback onPressed;
- final String text = 'Skip';
+  static const textColor = AppColors.primary500;
+  final VoidCallback onPressed;
+  final String text = 'Skip';
 
-const SkipButton({required this.onPressed , super.key});
+  const SkipButton({required this.onPressed, super.key});
 
-@override
-Widget build(BuildContext context){
-  return GestureDetector ( onTap: onPressed , child : Text(text , style: GoogleFonts.roboto( color : textColor , fontWeight : FontWeight.w400 , fontSize : 14.0 , height : 1.4 , letterSpacing : 0.0 ,)));
-
-
-
-
-}
-
-
-
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Text(text, style: AppTextStyles.bodyMediumMedium.copyWith(color: textColor)),
+    );
+  }
 }
