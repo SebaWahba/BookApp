4df520161fbd880bca16519a50b515498c17a07c
MarianMakram <marianmakram303@gmail.com>
2026-07-21 22:26:48 +0200

    implement Google Books API data layer, repository, and providers

---
+++ lib/features/books/data/models/book_model.dart
@@ -0,0 +1,35 @@
+class BookModel {
+  final String id;
+  final String title;
+  final List<String> authors;
+  final String description;
+  final String thumbnailUrl;
+  final double rating;
+
+  BookModel({
+    required this.id,
+    required this.title,
+    required this.authors,
+    required this.description,
+    required this.thumbnailUrl,
+    required this.rating,
+  });
+
+  factory BookModel.fromJson(Map<String, dynamic> json) {
+    final volumeInfo = json['volumeInfo'] as Map<String, dynamic>? ?? {};
+    final imageLinks = volumeInfo['imageLinks'] as Map<String, dynamic>?;
+
+    return BookModel(
+      id: json['id'] as String? ?? '',
+      title: volumeInfo['title'] as String? ?? 'No Title Available',
+      authors: (volumeInfo['authors'] as List<dynamic>?)
+          ?.map((e) => e.toString())
+          .toList() ??
+          ['Unknown Author'],
+      description: volumeInfo['description'] as String? ?? 'No description provided.',
+      thumbnailUrl: (imageLinks?['thumbnail'] as String? ?? '')
+          .replaceFirst('http://', 'https://'),
+      rating: (volumeInfo['averageRating'] as num?)?.toDouble() ?? 0.0,
+    );
+  }
+}
